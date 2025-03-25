76ef62d5faecf942ea3a231598bf8e71f9cc4dbe
onewangqianqian <59332478+onewangqianqian@users.noreply.github.com>
2025-03-25 20:03:35 +0800

    Delete BurstNet directory

--- BurstNet/cuda_check.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# 检查CUDA是否可用
-if torch.cuda.is_available():
-    print(f"CUDA is available. Number of GPUs: {torch.cuda.device_count()}")
-    print(f"Current GPU: {torch.cuda.get_device_name(torch.cuda.current_device())}")
-else:
-    print("CUDA is not available.")
